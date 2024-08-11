8bdf6c6a97065005d1de1118ab31c3918b67ae99
bertanj <mateusbertani@edu.unifil.br>
2024-08-11 15:51:03 -0300

    Finalização att matriz recursividade

--- src/Recursividade/MatrizRecursiva.java
+++ src/Recursividade/MatrizRecursiva.java
@@ -2,32 +2,27 @@ package Recursividade;
 
 import java.util.Scanner;
 
-public class MatrizRecursiva {
-    public static void matrizRecursiva(int [][]matriz, int indiceLinha, int indiceColuna){
-        //Condição de parada da matriz. Se a linha e a coluna forem igual ao tamanho final da linha e coluna da matriz
-        if(indiceLinha == matriz.length ){
-            return;
-        } else if (indiceColuna == matriz[0].length + indiceColuna) {
-            return;
+    public class MatrizRecursiva {
+        public static void matrizRecursiva(int [][]matriz, int indiceLinha, int indiceColuna){
+             if (indiceColuna == matriz[0].length) { //Quando chega ao limite máximo da coluna ele entra nessa condição e reinicia o indiceColuna e pula de linha
+                indiceColuna=0;//Reiniciando a contagem do indice da coluna
+                indiceLinha++;//Acrescentando 1 no indice linha para pular de linha
+                System.out.println();//Pulando de linha para ficar no formato da matriz
+            }
+            if(indiceLinha == matriz.length) { //Condição de parada
+                 return;
+            }
+
+            System.out.print(matriz[indiceLinha][indiceColuna]); //Printando os valores na tela
+            matrizRecursiva(matriz, indiceLinha, indiceColuna+1); //Chamada do método para acrescentar o indice coluna e percorrer todos os valores da coluna
         }
-        System.out.println(matriz[indiceLinha][indiceColuna]);
-        matrizRecursiva(matriz, indiceLinha , indiceColuna+1);
-    }
 
 
-   /* public static int matrizRecursiva(int [][]matriz, int indiceLinha, int indiceColuna){
-        if (indiceColuna == matriz.length){
-            return matriz[indiceLinha][indiceColuna];
-        }else if (indiceLinha == matriz.length-1){
-            return matriz[indiceLinha][indiceColuna];
-        }
-        return matrizRecursiva(matriz, indiceLinha, indiceColuna+1);
-    }
 
-    */
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int [][]matriz = new int[3][3];
+        int [][]matriz = new int[4][1];
 
         matriz[0][0] = 1;
         matriz[0][1] = 2;
@@ -39,14 +34,28 @@ public class MatrizRecursiva {
         matriz[2][1] = 8;
         matriz[2][2] = 9;
 
-        /*for(int i = 0; i < matriz.length; i++){
-            for(int j = 0; j < matriz[0].length; j++){
-                System.out.print(matriz[i][j]);
-            }
-            System.out.println();
-        }*/
-
         matrizRecursiva(matriz, 0, 0);
 
     }
 }
+
+ /*  antes estava fazendo assim e ficava em um looping infinito
+
+ public static void matrizRecursiva(int [][]matriz, int indiceLinha, int indiceColuna){
+            //Condição de parada da matriz. Se a linha e a coluna igualarem o tamanho final da linha e coluna da matriz
+            if(indiceLinha == matriz.length ){
+                return;
+            } else if (indiceColuna == matriz[0].length) {
+                return; //Quando entra nesse return o código pula a chamada do primeiro metodo, passa pelo sout de pular linha, zera o indice coluna e chama o metodo com o outro parametro para pular a linha e contar exibir as colunas novamente
+            }
+
+            System.out.print(matriz[indiceLinha][indiceColuna]);
+            matrizRecursiva(matriz, indiceLinha, indiceColuna+1);
+            System.out.println();
+
+            indiceColuna=0;//Reiniciando a contagem do indice da coluna
+            indiceLinha++;//Acrescentando 1 no indice linha para pular de linha
+
+            matrizRecursiva(matriz, indiceLinha, indiceColuna);
+
+        } */
